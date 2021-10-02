3d06ded6ddbd111428d4fcc1a27f627755c31eff
William Gauvin <william.gauvin@edu.cegepgarneau.ca>
2021-10-02 14:22:49 -0400

    wip weather now

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -9,16 +9,13 @@ use function Psy\debug;
 
 class WeatherController extends Controller
 {
-    //
-    public function getWeather()
+    public function getWeather($city)
     {
-        $weatherNow = Http::get('api.openweathermap.org/data/2.5/weather?q=Montreal&country code=124&units=metric&lang=fr&appid=e16e6f397ecd165a6d4c823042160975');
-        $weatherCity = json_decode($weatherNow);
-        dd($weatherCity->main->temp);
+        $weatherNow = Http::get('api.openweathermap.org/data/2.5/weather?q=Quebec&country code=124&units=metric&lang=fr&appid=e16e6f397ecd165a6d4c823042160975');
+        $weatherTemp = json_decode($weatherNow);
+        dd($weatherTemp->main->temp);
 
 
-        // return view("weatherNow", [
-        //     "weatherNow" => json_decode($weatherNow)
-        // ]);
+        return view('weatherNow', compact('city'));
     }
 }
